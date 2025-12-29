137e4fc79aae2babb20780f334a8aabcdff97175
Adi Takhar <adi.takhar@gmail.com>
2025-12-29 22:20:48 +0800

    bug fix

---
+++ models/dungeon_assets/sky_background/models/sky_background/models/sky_background/buildings/wall.gd
@@ -0,0 +1,7 @@
+extends StaticBody3D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	animation_player.play("moveWall")
---
+++ models/dungeon_assets/sky_background/models/sky_background/models/sky_background/models/sky_background/scenes/moving_wall.gd
@@ -0,0 +1,6 @@
+extends Node3D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	animation_player.play("moveWall")
